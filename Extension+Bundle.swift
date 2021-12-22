6d80673eea73f384fec6ca0ed37110885ac56eaa
Timm Kent <timm.kent@yahoo.com>
2021-12-22 15:32:38 +0100

    public vergessen

--- Extension+Bundle.swift
+++ Extension+Bundle.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension Bundle {
+public extension Bundle {
     var releaseVersionNumber: String? {
         return infoDictionary?["CFBundleShortVersionString"] as? String
     }
