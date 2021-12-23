9d352df512d115cd4b42d4ebe7ba7de59a2132ec
Timm Kent <timm.kent@yahoo.com>
2021-12-23 09:24:31 +0100

    storyboarded included

--- TKExtentionsToolKit/Classes/Extension+Bundle.swift
+++ TKExtentionsToolKit/Classes/Extension+Bundle.swift
@@ -18,3 +18,18 @@ public extension Bundle {
 }
 
 
+public protocol Storyboarded {
+    static func instantiate() -> Self
+}
+
+public extension Storyboarded where Self: UIViewController {
+    static func instantiate() -> Self {
+        let id = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        if #available(iOS 13.0, *) {
+            return storyboard.instantiateViewController(identifier: id) as! Self
+        } else {
+            fatalError("Wrong iOS Version")
+        }
+    }
+}
